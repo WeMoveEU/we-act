385d2b071bd37961baa84b0c0ffd440306159e1d
Romain Thouvenin <romain@wemove.eu>
2021-05-19 17:18:46 +0200

    Set-up requirements for headless tests

--- tests/phpunit/CRM/WeAct/BaseTest.php
+++ tests/phpunit/CRM/WeAct/BaseTest.php
@@ -22,6 +22,19 @@ use Civi\Test\TransactionalInterface;
 abstract class CRM_WeAct_BaseTest extends \PHPUnit\Framework\TestCase implements HeadlessInterface, HookInterface, TransactionalInterface {
 
   public function setUpHeadless() {
+    // Civi\Test has many helpers, like install(), uninstall(), sql(), and sqlFile().
+    // See: https://docs.civicrm.org/dev/en/latest/testing/phpunit/#civitest
+    return \Civi\Test::headless()
+      ->install(['eu.wemove.gidipirus', 'eu.wemove.contributm', 'org.project60.sepa'])
+      ->callback(function($ctx) {
+        civicrm_api3('PaymentProcessor', 'create', ['name' => 'CommitChange-card', 'payment_processor_type_id' => 'Dummy']);
+        civicrm_api3('PaymentProcessor', 'create', ['name' => 'CommitChange-sepa', 'payment_processor_type_id' => 'Dummy']);
+        //Sepa extension creates a Dummy creditor on install, but it doesn't have a type
+        civicrm_api3('Setting', 'create', ['batching_default_creditor' => 1]);
+      }, '2')  //Increase this value whenever modifying the callback so that the env builder detects the change
+      ->sql("UPDATE civicrm_sdd_creditor SET creditor_type = 'SEPA' WHERE creditor_type IS NULL")
+      ->installMe(__DIR__)
+      ->apply();
   }
 
   public function setUp() {
