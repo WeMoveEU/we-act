ddb098fa48708adba80d70468c90d02d881bf086
Aaron Elliot Ross <aaronelliotross@gmail.com>
2022-02-25 11:09:02 +0100

    Wowsers

--- tests/phpunit/CRM/WeAct/Page/StripeTest.php
+++ tests/phpunit/CRM/WeAct/Page/StripeTest.php
@@ -5,155 +5,205 @@ use CRM_WeAct_ExtensionUtil as E;
 /**
  * @group headless
  */
-class CRM_WeAct_Page_StripeTest extends CRM_WeAct_BaseTest
-{
+class CRM_WeAct_Page_StripeTest extends CRM_WeAct_BaseTest {
 
-    public function setUp(): void
-    {
-        parent::setUp();
-    }
+  public function setUp(): void {
+    parent::setUp();
+  }
 
-    public function testRefund()
-    {
-        // as long as we're using Proca to get the first donation, be sure
-        // to test using that processor!!
+  public function testRefund() {
+    // as long as we're using Proca to get the first donation, be sure
+    // to test using that processor!!
 
-        $action = CRM_WeAct_Action_ProcaTest::oneoffStripeAction();
+    $action = CRM_WeAct_Action_ProcaTest::oneoffStripeAction();
 
-        $charge_id = 'ch_vuhQqUhzksniV';
-        $action->details->paymentId = $charge_id;
+    $charge_id = 'ch_vuhQqUhzksniV';
+    $action->details->paymentId = $charge_id;
 
-        $processor = new CRM_WeAct_ActionProcessor();
-        $processor->processDonation(
-            $action, $this->campaignId, $this->contactId
-        );
+    $processor = new CRM_WeAct_ActionProcessor();
+    $processor->processDonation(
+      $action,
+      $this->campaignId,
+      $this->contactId
+    );
 
-        $refund = json_decode(file_get_contents("./tests/phpunit/events/charge-refunded.json"));
-        $refund->data->object->id = $charge_id;
+    $refund = json_decode(file_get_contents("./tests/phpunit/events/charge-refunded.json"));
+    $refund->data->object->id = $charge_id;
 
-        $page = new CRM_WeAct_Page_Stripe();
-        $page->processNotification($refund);
+    $page = new CRM_WeAct_Page_Stripe();
+    $page->processNotification($refund);
 
-        # find the contribution and check it's marked refunded
+    # find the contribution and check it's marked refunded
 
-        $contribution = civicrm_api3('Contribution', 'getsingle', ['trxn_id' => $charge_id]);
-        $this->assertTrue($contribution != NULL);
-        $this->assertEquals($contribution['contribution_status_id'], 7);
-    }
+    $contribution = civicrm_api3('Contribution', 'getsingle', ['trxn_id' => $charge_id]);
+    $this->assertTrue($contribution != NULL);
+    $this->assertEquals($contribution['contribution_status_id'], 7);
+  }
 
-    public function testSubscriptionCreate()
-    {
-        # create the customer
-        $customer_event = json_decode(file_get_contents("./tests/phpunit/events/customer.json"));
-        $page = new CRM_WeAct_Page_Stripe();
-        $page->processNotification($customer_event);
+  public function testSubscriptionCreate() {
 
-        # create the subscription
-        $subscription_event = json_decode(file_get_contents("./tests/phpunit/events/subscription.json"));
-        $page->processNotification($subscription_event);
+    // TODO : test with a create customer
 
-        $subscription = $subscription_event->data->object;
-        $recurring = civicrm_api3('ContributionRecur', 'get', ['trxn_id' => $subscription->id]);
-        $this->assertTrue($recurring != NULL);
-    }
+    // # create the customer
+    $customer_event = json_decode(file_get_contents("./tests/phpunit/events/customer.json"));
+    $page = new CRM_WeAct_Page_Stripe();
+    $contact = $page->processNotification($customer_event);
 
-    public function testSubscriptionUpdateAmount()
-    {
-        $subscription = "sub_uqWHvXgyuwzTQ";
-        $amount = 2000;
-        $action = CRM_WeAct_Action_ProcaTest::recurringStripeAction(
-            'monthly', null, $subscription, $amount
-        );
+    $contact_id = $contact['id'];
+    $_ENV['testing_contact_id'] = $contact_id;
 
-        // print(json_encode($action)); # // details->amount
+    # create the contribution_recur (and the contact if needed, which it is here)
+    $subscription_event = json_decode(file_get_contents("./tests/phpunit/events/subscription.json"));
+    $page->processNotification($subscription_event);
 
-        $processor = new CRM_WeAct_ActionProcessor();
-        $processor->processDonation($action, $this->campaignId, $this->contactId);
+    $subscription = $subscription_event->data->object;
+    $recurring = civicrm_api3('ContributionRecur', 'getsingle', ['trxn_id' => $subscription->id]);
+    $this->assertTrue($recurring != NULL);
+    $this->assertEquals($recurring['contact_id'], $contact_id);
 
-        $contribution = civicrm_api3(
-            'ContributionRecur',
-            'getsingle',
-            ['trxn_id' => $subscription]
-        );
-        $this->assertEquals("20.00", $contribution['amount']);
+    $charge = civicrm_api3(
+      'Contribution',
+      'getsingle',
+      ['contribution_recur_id' => $recurring['id']]
+    );
+    $this->assertEquals($charge['contact_id'], $contact_id);
+    $this->assertEquals(
+        substr($charge['trxn_id'], 0, 3), 'ch_'
+      );
 
-        $amount = 4000;
+    unset($_ENV['testing_contact_id']);
+  }
 
-        $stripe_event = $this->customerSubscriptionUpdatedEvent(
-            $subscription,
-            $amount,
-        );
+  public function testSubscriptionUpdateAmount() {
+    $subscription = "sub_uqWHvXgyuwzTQ";
+    $amount = 2000;
+    $action = CRM_WeAct_Action_ProcaTest::recurringStripeAction(
+      'monthly',
+      null,
+      $subscription,
+      $amount
+    );
 
-        $page = new CRM_WeAct_Page_Stripe();
-        $page->processNotification(json_decode($stripe_event));
+    // print(json_encode($action)); # // details->amount
 
-        $contribution = civicrm_api3(
-            'ContributionRecur', 'getsingle', ['trxn_id' => $subscription]
-        );
+    $processor = new CRM_WeAct_ActionProcessor();
+    $processor->processDonation($action, $this->campaignId, $this->contactId);
 
-        $this->assertEquals("40.00", $contribution['amount']);
-    }
+    $contribution = civicrm_api3(
+      'ContributionRecur',
+      'getsingle',
+      ['trxn_id' => $subscription]
+    );
+    $this->assertEquals("20.00", $contribution['amount']);
 
-    // Test update status
+    $amount = 4000;
 
-    public function testSubscriptionCancel()
-    {
+    $stripe_event = $this->customerSubscriptionUpdatedEvent(
+      $subscription,
+      $amount,
+    );
 
-        // ... test the subscription is canceled changes ...
+    $page = new CRM_WeAct_Page_Stripe();
+    $page->processNotification(json_decode($stripe_event));
 
-    }
+    $contribution = civicrm_api3(
+      'ContributionRecur',
+      'getsingle',
+      ['trxn_id' => $subscription]
+    );
 
-    public function testNewinvoicePaymentSucceededEvent()
-    {
-        $subscription = "sub_AccmDyDhCXVvJtXf";
-        $action = CRM_WeAct_Action_ProcaTest::recurringStripeAction('monthly', null, $subscription);
-        $processor = new CRM_WeAct_ActionProcessor();
-        $processor->processDonation($action, $this->campaignId, $this->contactId);
+    $this->assertEquals("40.00", $contribution['amount']);
+  }
 
-        $invoice = "in_sZJpHDMwEnahHziF";
-        $charge = "ch_rgWvkHQrADdcgPdY";
-        $payment_intent = "pi_uFoDjxBJLjaZpqBn";
+  // Test update status
 
-        $created = '2022-01-28 01:34:00';
-        $created_dt = new DateTime($created);
+  public function testSubscriptionCancel() {
 
-        $stripe_event = $this->invoicePaymentSucceededEvent(
-            $subscription,
-            $invoice,
-            $charge,
-            $payment_intent,
-            $created_dt->getTimestamp()
-        );
+    $subscription = "sub_1KQYMRLEJyfuWvBB831StfM3";
+    $amount = 1400;
+    $action = CRM_WeAct_Action_ProcaTest::recurringStripeAction(
+      'monthly',
+      null,
+      $subscription,
+      $amount
+    );
 
-        $page = new CRM_WeAct_Page_Stripe();
-        $page->processNotification(json_decode($stripe_event));
+    // print(json_encode($action)); # // details->amount
 
-        $contribution = civicrm_api3('Contribution', 'getsingle', ['trxn_id' => $invoice]);
-        $this->assertEquals($contribution['receive_date'], $created);
-        $this->assertEquals($contribution['contribution_status_id'], 1); # Completed
+    $processor = new CRM_WeAct_ActionProcessor();
+    $processor->processDonation($action, $this->campaignId, $this->contactId);
 
-        # do it again, make sure we don't add it again
-        $page->processNotification(json_decode($stripe_event));
-        $contributions = civicrm_api3('Contribution', 'get', ['trxn_id' => $invoice]);
-        $this->assertEquals($contributions['count'], 1);
-    }
+    $contribution = civicrm_api3(
+      'ContributionRecur',
+      'getsingle',
+      ['trxn_id' => $subscription]
+    );
+    $this->assertEquals("14.00", $contribution['amount']);
 
-    public function testUnknownEvent()
-    {
-        $page = new CRM_WeAct_Page_Stripe();
-        $event = <<<JSON
+    $cancellation = json_decode(file_get_contents("./tests/phpunit/events/subscription-cancelled.json"));
+    $cancellation->data->object->id = $subscription;
+
+    $page = new CRM_WeAct_Page_Stripe();
+    $page->processNotification($cancellation);
+
+    $contribution = civicrm_api3(
+      'ContributionRecur',
+      'getsingle',
+      ['trxn_id' => $subscription]
+    );
+    $this->assertTrue($contribution != NULL);
+    $this->assertEquals($contribution['contribution_status_id'], 3);
+    $this->assertNotEquals($contribution['cancel_date'], NULL);
+  }
+
+  public function testNewinvoicePaymentSucceededEvent() {
+    $subscription = "sub_AccmDyDhCXVvJtXf";
+    $action = CRM_WeAct_Action_ProcaTest::recurringStripeAction('monthly', null, $subscription);
+    $processor = new CRM_WeAct_ActionProcessor();
+    $processor->processDonation($action, $this->campaignId, $this->contactId);
+
+    $invoice = "in_sZJpHDMwEnahHziF";
+    $charge = "ch_rgWvkHQrADdcgPdY";
+    $payment_intent = "pi_uFoDjxBJLjaZpqBn";
+
+    $created = '2022-01-28 01:34:00';
+    $created_dt = new DateTime($created);
+
+    $stripe_event = $this->invoicePaymentSucceededEvent(
+      $subscription,
+      $invoice,
+      $charge,
+      $payment_intent,
+      $created_dt->getTimestamp()
+    );
+
+    $page = new CRM_WeAct_Page_Stripe();
+    $page->processNotification(json_decode($stripe_event));
+
+    $contribution = civicrm_api3('Contribution', 'getsingle', ['trxn_id' => $invoice]);
+    $this->assertEquals($contribution['receive_date'], $created);
+    $this->assertEquals($contribution['contribution_status_id'], 1); # Completed
+
+    # do it again, make sure we don't add it again
+    $page->processNotification(json_decode($stripe_event));
+    $contributions = civicrm_api3('Contribution', 'get', ['trxn_id' => $invoice]);
+    $this->assertEquals($contributions['count'], 1);
+  }
+
+  public function testUnknownEvent() {
+    $page = new CRM_WeAct_Page_Stripe();
+    $event = <<<JSON
       { "type": "invoice.idontexist", "id": "evt_ineverheardof" }
 JSON;
-        $page->processNotification(json_decode($event));
-        $this->assertTrue(true);
-    }
+    $page->processNotification(json_decode($event));
+    $this->assertTrue(true);
+  }
 
-    // TODO: Unknown subscription - But what should it do? Create the subscription right?
-    // public function testUnknownRecurringDonation() {}
+  // TODO: Unknown subscription - But what should it do? Create the subscription right?
+  // public function testUnknownRecurringDonation() {}
 
-    protected function invoicePaymentSucceededEvent($subscription, $invoice, $charge, $payment_intent, $created)
-    {
-        return <<<JSON
+  protected function invoicePaymentSucceededEvent($subscription, $invoice, $charge, $payment_intent, $created) {
+    return <<<JSON
         {
         "id": "evt_1KRJgJLEJyfuWvBBiCLjk75H",
         "object": "event",
@@ -188,11 +238,10 @@ JSON;
         "type": "invoice.payment_succeeded"
         }
 JSON;
-    }
+  }
 
-    protected function customerSubscriptionUpdatedEvent($subscription, $amount)
-    {
-        return <<<JSON
+  protected function customerSubscriptionUpdatedEvent($subscription, $amount) {
+    return <<<JSON
     {
       "id": "evt_1KRJgJLEJyfuWvBBiCLjk75H",
       "object": "event",
@@ -237,6 +286,5 @@ JSON;
       "type": "customer.subscription.updated"
     }
 JSON;
-    }
-
+  }
 }
